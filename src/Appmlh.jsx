2d8412370d46a053e0e722322f89bcd0b010f2b9
Sekseos <haddon.mike2@outlook.com>
2020-05-11 16:33:10 -0500

    Make Images fetched from IMGUR

--- src/Appmlh.jsx
+++ src/Appmlh.jsx
@@ -1,24 +1,28 @@
 import React from 'react';
 import imgScroller from './Component/imgScroller';
 import './Appmlh.css';
+// import Axios from 'axios';
 
 class App extends React.Component{
   constructor(props){
     super(props)
     this.state = {
-      currImage: imgScroller.imageHead
+      currImage: 'https://i.imgur.com/zM0MQFD.jpg',
+      images: [
+        'https://i.imgur.com/zM0MQFD.jpg',
+        'https://i.imgur.com/R9JVRsP.jpg',
+        'https://i.imgur.com/wuuIRSv.jpg',
+        'https://i.imgur.com/0i8iBU7.jpg',
+        'https://i.imgur.com/9UBZ6bp.jpg',
+        'https://i.imgur.com/oZu7p66.jpg'
+      ]
     }
-    this.handleHover.bind(this);
   }
 
   componentDidMount() {
-    this.setState({currImage: imgScroller.imageHead})
+
   }
 
-  handleHover(imgHovered) {
-    console.log('Hovered Over Image File');
-    this.setState({currImage: imgHovered});
-  };
 
   render() {
     return (
@@ -27,13 +31,13 @@ class App extends React.Component{
           <thead id='main-image'>
             <tr>
               <td id='HeaderImg'>
-                {this.state.currImage}
+                <img src={this.state.currImage} alt={this.state.images[0]} className='imgHead'></img>
               </td>
             </tr>
           </thead>
           <tbody id='imgList'>
             <tr id='imgbuttons'>
-              <imgScroller.Scroller />
+              <imgScroller.Scroller images={this.state.images}/>
             </tr>
           </tbody>
         </table>
