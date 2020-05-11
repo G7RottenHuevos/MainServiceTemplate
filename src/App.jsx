690e6651ad89eb40ea1e9e227575ffc8867e6b29
Paul <pprokopy@gmail.com>
2020-05-11 11:34:21 -0500

    11 may FEC

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
 //import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Container from './Components/Container';
-import './App.css';
+import './PaulApp.css';
 
 class App extends Component{
   constructor(props){
