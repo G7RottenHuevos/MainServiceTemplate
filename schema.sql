e30f6d21e6645512e125578544b24199c4499d27
D9SE <daltonsoohoo9@gmail.com>
2020-05-14 10:13:00 -0500

    header

--- schema.sql
+++ schema.sql
@@ -18,31 +18,21 @@ CREATE TABLE headerResults (
   PRIMARY KEY (ID)
 );
 
-INSERT INTO header (id, name) VALUES (1, "Albrey");
-INSERT INTO header (id, name) VALUES (2, "Allen");
-INSERT INTO header (id, name) VALUES (3, "Asif");
-INSERT INTO header (id, name) VALUES (4, "Beth");
-INSERT INTO header (id, name) VALUES (5, "Cody");
-INSERT INTO header (id, name) VALUES (6, "Dan");
-INSERT INTO header (id, name) VALUES (7, "Fred");
-INSERT INTO header (id, name) VALUES (8, "Hailey");
-INSERT INTO header (id, name) VALUES (9, "Heather");
-INSERT INTO header (id, name) VALUES (10, "Joel");
-INSERT INTO header (id, name) VALUES (11, "John");
-INSERT INTO header (id, name) VALUES (12, "Kan");
-INSERT INTO header (id, name) VALUES (13, "Linden");
-INSERT INTO header (id, name) VALUES (14, "Liz");
-INSERT INTO header (id, name) VALUES (15, "Magee");
-INSERT INTO header (id, name) VALUES (16, "Marcus");
-INSERT INTO header (id, name) VALUES (17, "Marlene");
-INSERT INTO header (id, name) VALUES (18, "Mohammed");
-INSERT INTO header (id, name) VALUES (19, "Mylani");
-INSERT INTO header (id, name) VALUES (20, "Omar");
-INSERT INTO header (id, name) VALUES (21, "Rebecca");
-INSERT INTO header (id, name) VALUES (22, "Robin");
-INSERT INTO header (id, name) VALUES (23, "Sophie");
-INSERT INTO header (id, name) VALUES (24, "Tom");
-
+INSERT INTO header (id, name) VALUES (1, "Facemask");
+INSERT INTO header (id, name) VALUES (2, "Hand Sanitzer");
+INSERT INTO header (id, name) VALUES (3, "Toilet Paper");
+INSERT INTO header (id, name) VALUES (4, "Lysol");
+INSERT INTO header (id, name) VALUES (5, "Meat");
+INSERT INTO header (id, name) VALUES (6, "Bread");
+INSERT INTO header (id, name) VALUES (7, "Garlic");
+INSERT INTO header (id, name) VALUES (8, "Tissues");
+INSERT INTO header (id, name) VALUES (9, "Water");
+INSERT INTO header (id, name) VALUES (10, "Milk");
+INSERT INTO header (id, name) VALUES (11, "Juice");
+INSERT INTO header (id, name) VALUES (12, "First Aid Kit");
+INSERT INTO header (id, name) VALUES (13, "Apples");
+INSERT INTO header (id, name) VALUES (14, "Orange");
+INSERT INTO header (id, name) VALUES (15, "Watermelon");
 
 INSERT INTO headerResults (from_student, to_student, preference_type) VALUES (1, 2, "YES");
 INSERT INTO headerResults (from_student, to_student, preference_type) VALUES (1, 3, "YES");
@@ -59,10 +49,4 @@ INSERT INTO headerResults (from_student, to_student, preference_type) VALUES (1,
 INSERT INTO headerResults (from_student, to_student, preference_type) VALUES (1, 14, "YES");
 INSERT INTO headerResults (from_student, to_student, preference_type) VALUES (1, 15, "YES");
 INSERT INTO headerResults (from_student, to_student, preference_type) VALUES (1, 16, "YES");
-INSERT INTO headerResults (from_student, to_student, preference_type) VALUES (1, 17, "YES");
-INSERT INTO headerResults (from_student, to_student, preference_type) VALUES (1, 18, "YES");
-INSERT INTO headerResults (from_student, to_student, preference_type) VALUES (1, 19, "YES");
-INSERT INTO headerResults (from_student, to_student, preference_type) VALUES (1, 20, "YES");
-INSERT INTO headerResults (from_student, to_student, preference_type) VALUES (1, 21, "YES");
-INSERT INTO headerResults (from_student, to_student, preference_type) VALUES (1, 22, "YES");
-INSERT INTO headerResults (from_student, to_student, preference_type) VALUES (1, 23, "YES");
+
